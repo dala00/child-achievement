3defefa8f2b524a09656df9519f942b25ffd24f6
User Alphabrend <alphabrend@www2727.sakura.ne.jp>
2017-11-17 00:07:56 +0900

    Update title

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => '幼稚園達成ボード',
 
     /*
     |--------------------------------------------------------------------------
